258989bccd6594ce4f65627ac8f85cf55cef25ff
Yuri Colombari Miguel <yuri.colombari@hotmail.com>
2024-05-03 00:18:27 -0300

    sincronizar

--- ProjectAmericaNews/obj/Debug/net8.0/ProjectAmericaNews.RazorAssemblyInfo.cs
+++ ProjectAmericaNews/obj/Debug/net8.0/ProjectAmericaNews.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AmericaNews.Data")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("AmericaNews.Data")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AmericaNews.Data")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
