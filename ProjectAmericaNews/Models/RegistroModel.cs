403a70a5112e16ed1576ae8020548b17add7c6c0
jeferson freitas da silva <98712214+JefersonFreitasdaSilva@users.noreply.github.com>
2024-05-08 21:05:16 -0300

    Models mod 1 finalizado

---
+++ ProjectAmericaNews/Models/RegistroModel.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Registro
+{ 
+    public int ID { get; set; }
+    public string Tabela { get; set; }
+    public string Coluna { get; set; }
+    public string Antes {  get; set; }
+    public string Depois { get; set; }
+    public DateTime Data {  get; set; }
+
+    public Registro(string tabela,string coluna,string antes,string depois,DateTime data) 
+    {
+        Tabela=tabela;
+        Coluna=coluna;
+        Antes=antes;
+        Depois=depois;
+        Data = data;
+    }
+
+    public Registro() 
+    { }
+
+    public override string ToString()
+    {
+        return $"ID: {ID}, Tabela: {Tabela}, Coluna: {Coluna}, Antes: {Antes}, Depois: {Depois}, Data: {Data}"
+    }
+}
