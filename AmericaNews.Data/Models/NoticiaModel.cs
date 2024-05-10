15b82e3c31efed10f1f05804e622dc6981019449
Beatriz dos Santos Silva <beatriz.dssilva3@gmail.com>
2024-05-10 07:16:05 -0300

    alteração do caminho das models

--- AmericaNews.Data/Models/NoticiaModel.cs
+++ AmericaNews.Data/Models/NoticiaModel.cs
@@ -1,21 +1,37 @@
-ï»¿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace AmericaNews.Data.Models
+public class NoticiaModel
 {
-    public class NoticiaModel
+    public int ID { get; set; }
+    public string Titulo { get; set; }
+    public string? Subtitulo { get; set; }
+    public string Texto { get; set; }
+    public DateTime Data { get; set; }
+    public bool? Ocultar { get; set; }
+    public int IDUsuario { get; set; }
+    public int? ID_ADM_Aprovou { get; set; }
+    public DateTime? DataAprovada { get; set; }
+
+    public NoticiaModel()
+    { }
+
+    public NoticiaModel(string titulo, string? subtitulo, string texto, DateTime data, bool? ocultar, int idUsuario, int? idAdmAprovou, DateTime? dataAprovada)
+    {   
+        Titulo = titulo;
+        Subtitulo = subtitulo;
+        Texto = texto;
+        Data = data;
+        Ocultar = ocultar;
+        IDUsuario = idUsuario;
+        ID_ADM_Aprovou = idAdmAprovou;
+        DataAprovada = dataAprovada;
+    }
+
+    public override string ToString()
     {
-        public int ID { get; set; }
-        public string Titulo { get; set; }
-        public string Subtitulo { get; set; }
-        public string Texto { get; set; }
-        public DateTime Data { get; set; }
-        public bool Ocultar { get; set; }
-        public int IDUsuario { get; set; }
-        public int? ID_ADM_Aprovou { get; set; }
-        public DateTime? DataAprovada { get; set; }
+        string admAprovouInfo = ID_ADM_Aprovou != 0 ? ID_ADM_Aprovou.ToString() : "N/A";
+        string dataAprovadaInfo = DataAprovada.HasValue ? DataAprovada.Value.ToString() : "N/A";
+
+        return $"ID: {ID}, Título: {Titulo}, Subtítulo: {Subtitulo}, Texto: {Texto}, Data: {Data}, Ocultar: {Ocultar}, IDUsuario: {IDUsuario}, ID_ADM_Aprovou: {admAprovouInfo}, DataAprovada: {dataAprovadaInfo}";
     }
 }
